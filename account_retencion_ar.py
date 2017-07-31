c3e8becf319c0307869c41ded888643e8dc2103e
Adrián Bernardi <adrian.bernardi@gmail.com>
2017-07-31 18:50:31 -0300

    Fixes + flake8

--- account_retencion_ar.py
+++ account_retencion_ar.py
@@ -1,7 +1,8 @@
-#This file is part of the account_retencion_ar module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part of the account_retencion_ar module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from decimal import Decimal
+
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pyson import Eval, Bool, Not
 from trytond.pool import Pool
